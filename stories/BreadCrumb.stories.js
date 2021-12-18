909c20ca5e5755b05e85e8e59e22dfa9693ff32e
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-12-18 20:44:07 +0900

    パンくず追加・ボタンレイアウト変更

---
+++ stories/BreadCrumb.stories.js
@@ -0,0 +1,18 @@
+import { createbreadcrumb } from './template/breadcrumb';
+
+// More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
+export default {
+  title: 'Component/BreadCrumb',
+  // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
+};
+
+// More on component templates: https://storybook.js.org/docs/html/writing-stories/introduction#using-args
+const Template = ({ label, ...args }) => {
+  return createbreadcrumb({ label, ...args });
+};
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/html/writing-stories/args
+Default.args = {
+  classname: 'default',
+};
---
+++ stories/template/breadcrumb.js
@@ -0,0 +1,19 @@
+import './../css/breadcrumb.css';
+
+export const createbreadcrumb = ({
+}) => {
+  const nav = document.createElement('nav');
+
+  const breasbrumb = `
+    <ol class="c-breadcrumb_inner">
+      <li class="c-breadcrumb_item"><a href="#">Home</a></li>
+      <li class="c-breadcrumb_item"><a href="#">Library</a></li>
+      <li class="c-breadcrumb_item" aria-current="page">Data</li>
+    </ol>
+  `;
+  nav.insertAdjacentHTML('afterbegin', breasbrumb);
+
+  nav.className = ['c-breadbrumb'].join(' ');
+
+  return nav;
+};
