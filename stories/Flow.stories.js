6bebc5bfe22822756f98112988127a583f0b06ad
uemuragame5683 <nobuyoshi5683@gmail.com>
2022-01-22 01:17:03 +0900

    flow追加・各所レイアウト調整

--- stories/Flow.stories.js
+++ stories/Flow.stories.js
@@ -1,24 +1,18 @@
-import { createnav } from './template/Nav';
+import { createflow } from './template/Flow';
 
 // More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
 export default {
-  title: 'Component/Nav',
+  title: 'Component/Flow',
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
 };
 
 // More on component templates: https://storybook.js.org/docs/html/writing-stories/introduction#using-args
 const Template = ({ label, ...args }) => {
-  return createnav({ label, ...args });
+  return createflow({ label, ...args });
 };
 
 export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/html/writing-stories/args
 Default.args = {
   classname: 'default',
-};
-
-export const Focus = Template.bind({});
-// More on args: https://storybook.js.org/docs/html/writing-stories/args
-Focus.args = {
-  classname: 'focus',
 };
